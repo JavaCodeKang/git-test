d0a2352f6e15d2cb18dd36a1963159c929b4fc8a
陋室孤灯 <m18832006432@163.com>
2021-07-22 22:17:45 +0800

    third commit

--- src/main/java/com/giTtest/git/Git.java
+++ src/main/java/com/giTtest/git/Git.java
@@ -8,5 +8,7 @@ package com.giTtest.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int i = 0;
+        System.out.println(i++);
     }
 }
